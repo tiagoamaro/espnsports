44017e24ad1efc0c82a1a6cae49807b753e7de15
Tiago Amaro <tiagocis@gmail.com>
2015-04-12 14:41:30 -0300

    Creating seed file to auto generate tasks for available leagues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#########
+# Leagues
+#########
+
+# Array of valid leagues for the SportsScraper. This structure is an Array of Arrays, where the elements are:
+# id, League Name, Sport Friendly name
+leagues = [[1, "NBA", "Basketball"],
+ [nil, "NASCAR", "Racing"],
+ [3, "NFL", "Football"],
+ [nil, "NCB", "Basketball"],
+ [nil, "NCW", "Basketball"],
+ [8, "WNBA", "Basketball"],
+ [7, "NCF", "Football"],
+ [9, "MLS", "Soccer"],
+ [6, "PGA", "Golf"],
+ [4, "MLB", "Baseball"],
+ [5, "NHL", "Hockey"]]
+
+leagues.each do |league|
+  League.find_or_create_by('LeagueName' => league[0], 'LeagueName' => league[1], 'SportName' => league[2])
+end
+
+#######
+# Tasks
+#######
+
+Task.find_each { |t| t.stop! }
+League.find_each do |league|
+  Task.find_or_create_by(league_name: league.LeagueName)
+end
